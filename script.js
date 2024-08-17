373a86929ecc9335be7b0600cfe6b84d1b38e2ea
Rinku Dhurua <rinkudhurua258@gmail.com>
2024-08-17 22:29:02 +0530

    added new song

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ tracks = [
         cover: "https://www.drivespark.com/img/2024/06/kalki-2898-bujji-robot-car-cover-1719471662.jpg",
         source: "./music/Bujji Theme Music.mp3",
     },
+    {
+        name: "YODHA_ Tiranga",
+        artist: " B Praak",
+        cover: "./audiopic/tiranga.jpg",
+        source: "./music/YODHA_ Tiranga.mp3",
+    },
+    {
+        name: "Bhairava Anthem",
+        artist: "diljit dosanjh",
+        cover: "./audiopic/Bhairava.jpg",
+        source: "./music/Bhairava Anthem.mp3",
+    },
 
     {
         name: "Forever",
@@ -22,6 +34,31 @@ tracks = [
         artist: "Gryffin ",
         cover: "./audiopic/Griffyn_(43228990201).jpg",
         source: "./music/Gryffin - Just For A Moment ft. Iselin [TubeRipper.com].m4a",
+    },
+    {
+        name: "Aayi Nai",
+        artist: " Sachin-Jigar,Pawan Singh,Simran,Divya,Amitabh  ",
+        cover: "./audiopic/ayi nahi.jpg",
+        source: "./music/Aayi Nai.mp3",
+    },
+
+    {
+        name: "Khoobsurat",
+        artist: " Vishal Mishra, Amitabh Bhattacharya,Sachin-Jigar",
+        cover: "./audiopic/Khubsurat.png",
+        source: "./music/Khoobsurat  Stree 2.mp3",
+    },
+    {
+        name: "Nachde Ne Saar",
+        artist: "Jasleen Royal,Harshdeep Kaur,Siddharth Mahadevan",
+        cover: "./audiopic/nachde.jpg",
+        source: "./music/Nachde Ne Saar.mp3",
+    },
+    {
+        name: "Maangalyam",
+        artist: "Vijay Yesudas",
+        cover: "./audiopic/Maangyalam.jpg",
+        source: "./music/Maangalyam.mp3",
     },
 
     {
@@ -43,6 +80,12 @@ tracks = [
         source: "./music/PEACE  RINGTONE.mp3",
     },
     {
+        name: "RaghuNandana",
+        artist: "Bhaskaruni Sai Charan,okeshwar Edara,",
+        cover: "./audiopic/raghu.jpg",
+        source: "./music/Raghunandana A Powerful Vandana.mp3",
+    },
+    {
         name: "Kaththi X Jawan",
         artist: "Fans ",
         cover: "./audiopic/kaithi.jpg",
@@ -53,6 +96,36 @@ tracks = [
         artist: "Unknown",
         cover: "",
         source: "./music/Ringtone.mp3",
+    },
+    {
+        name: "Oo Mago Turu Love",
+        artist: "Bunty R Samal",
+        cover: "./audiopic/omago.jpg",
+        source: "./music/Oo Mago Turu Lob.mp3",
+    },
+    {
+        name: "Oo Mago Turu Love(Female Version)",
+        artist: "Bunty R Samal and Aseema Panda",
+        cover: "./audiopic/f omago.jpg",
+        source: "./music/OO MagoTuru Lob (Female Version).mp3",
+    },
+    {
+        name: "Gariba Pila",
+        artist: " Smruti R & Bunty R Samal",
+        cover: "./audiopic/gariba.jpg",
+        source: "./music/Gariba Pila.mp3",
+    },
+    {
+        name: "ମୁଁ ଫେରିଲେ ତମର",
+        artist: " Mantu Chhuria,Aseema Panda",
+        cover: "./audiopic/pherile.jpg",
+        source: "./music/ମୁଁ ଫେରିଲେ  ତମର.mp3",
+    },
+    {
+        name: "Lajabati Lajabarana",
+        artist: " Abinash Dash",
+        cover: "./audiopic/lajabati.jpg",
+        source: "./music/Lajabati Lajabarana.mp3",
     },
 
 ];
